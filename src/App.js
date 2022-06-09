bc2e2f5f32408b17fbe370ce1a76651560464d31
Salam Sheikh <dev.salamsheikh@gmail.com>
2022-06-10 02:30:36 +0600

    github rest api website using react js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import "./App.css";
+import { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
 
 function App() {
+  const [avatarUrl, setAvatarUrl] = useState();
+  const [githubUsername, setGithubUsername] = useState();
+  const [bio, setBio] = useState();
+  const [followers, setFollowers] = useState();
+  const [siteUrl, setSiteUrl] = useState();
+  const [location, setLocation] = useState();
+  const [repo, setRepo] = useState();
+  const [repoData, setRepoData] = useState();
+
+  async function repoDataURL() {
+    fetch("https://api.github.com/users/Dev-SalamSheikh/repos")
+      .then((res) => res.json())
+      .then((result) => {
+        const list = result.map((item, index) => (
+          <div key={index}>
+            <Card
+              border="danger"
+              text="white"
+              bg="dark"
+              style={{ width: "15rem" }}
+            >
+              <Card.Body>
+                <Card.Title>{item.full_name}</Card.Title>
+                <Card.Text>
+                  {item.description
+                    ? item.description
+                    : "Description is not avaiable in this Repository"}
+                </Card.Text>
+                <Card.Text>
+                  Repository Link <a href={item.clone_url}>{item.clone_url}</a>
+                </Card.Text>
+                <Card.Text style={{ color: "red" }}>
+                  Default Branch: {item.default_branch}
+                </Card.Text>
+                <Card.Text style={{ color: "lightblue" }}>
+                  Most Used Language: {item.language}
+                </Card.Text>
+                <Card.Text>Repo Froks: {item.forks_count}</Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
+        ));
+        setRepoData(list);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/Dev-SalamSheikh")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setAvatarUrl(result.avatar_url);
+          setGithubUsername(result.login);
+          setBio(result.bio);
+          setFollowers(result.followers);
+          setSiteUrl(result.blog);
+          setLocation(result.location);
+          setRepo(result.public_repos);
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container className="pb-5">
+        <div className="App w-100 min-vh-100 d-flex align-items-center flex-wrap justify-content-center gap-4">
+          <div className="github_profile_pic">
+            <img src={avatarUrl} alt="ProfileImage" />
+          </div>
+          <div className="github_statistics">
+            <h1>
+              <span>github.com/</span>
+              {githubUsername}
+            </h1>
+            <span className="bio">{bio}</span>
+            <p className="pt-3 pb-0">Total Followers - {followers}</p>
+            <a href="https://salamsheikh.me">Personal Portfolio - {siteUrl}</a>
+            <h5 className="lcoation">From - {location}</h5>
+            <p
+              style={{ color: "red", fontSize: "24px" }}
+              className="total_repo fw-bold"
+            >
+              Total Public Repository - {repo}
+            </p>
+            <Button variant="primary" onClick={repoDataURL}>
+              Show My Public Repository
+            </Button>
+          </div>
+        </div>
+
+        <div className="mt-5 d-flex flex-row flex-wrap align-items-center justify-content-center text-center gap-5">
+          {repoData}
+        </div>
+      </Container>
+    </>
   );
 }
 
